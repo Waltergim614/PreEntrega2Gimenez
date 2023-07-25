d9df96107416a900709a47328fcc590c2090dd63
Walter <walygim@gmail.com>
2023-07-25 01:42:57 -0300

    mas contenido al index, uso de libreria de animaciones, correcciones en media queris y boton modo obscuro en menu hamburguesa

--- js/mododark.js
+++ js/mododark.js
@@ -3,22 +3,27 @@ const btnSwitch2 = document.querySelector("#switch2");
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
 const main = document.querySelector("main");
-function cambiarModo() {
-    document.body.classList.toggle("dark");
-    header.classList.toggle("dark");
-    footer.classList.toggle("dark");
-    main.classList.toggle("dark");
-    btnSwitch.classList.toggle("active");
+
+const isDarkMode = localStorage.getItem("darkMode") === "true";
+if (isDarkMode) {
+    document.body.classList.add("dark");
+    header.classList.add("dark");
+    footer.classList.add("dark");
+    main.classList.add("dark");
+    btnSwitch.classList.add("active");
+    btnSwitch2.classList.add("active");
 }
 
-function cambiarModo2() {
-    document.body.classList.toggle("dark");
+function cambiarModo() {
+    main.classList.toggle("dark");
     header.classList.toggle("dark");
     footer.classList.toggle("dark");
-    main.classList.toggle("dark");
     btnSwitch.classList.toggle("active");
-    
+    btnSwitch2.classList.toggle("active");
+
+    const isDarkMode = main.classList.contains("dark");
+    localStorage.setItem("darkMode", isDarkMode.toString());
 }
 
 btnSwitch.addEventListener("click", cambiarModo);
-btnSwitch2.addEventListener("click", cambiarModo2);
+btnSwitch2.addEventListener("click", cambiarModo);
